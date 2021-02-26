381f9146d24acdd26ae050ded6b8049a9b7d5450
IronDeagle07 <79700968+IronDeagle07@users.noreply.github.com>
2021-02-26 08:29:32 -0400

    Formes Project 1

--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
-basic.forever(function () {
-	
+let FORMES = 0
+input.onGesture(Gesture.Shake, function () {
+    FORMES = randint(0, 3)
+    if (FORMES == 0) {
+        basic.showLeds(`
+            # # # # #
+            # . . . #
+            # . . . #
+            # . . . #
+            # # # # #
+            `)
+        basic.showString("Merci!")
+    } else if (FORMES == 1) {
+        basic.showLeds(`
+            . # # # .
+            . # . # .
+            . # . # .
+            . # . # .
+            . # # # .
+            `)
+        basic.showString("Merci!")
+    } else if (FORMES == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . # . .
+            . # . # .
+            # # # # #
+            . . . . .
+            `)
+        basic.showString("Merci!")
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . . . #
+            . # . # .
+            . . # . .
+            `)
+        basic.showString("Merci!")
+    }
 })
